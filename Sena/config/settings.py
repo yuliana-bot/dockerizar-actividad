49da6e45065cc4d10aeb11a049e8d5e7552a44f3
Angie Yuliana Pedraza Barrera <angiey_pedrazaq@soy.sena.edu.co>
2026-07-02 11:04:34 -0500

    hsadhsahdsa

--- Sena/config/settings.py
+++ Sena/config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$u@kk!ne+y*gg=^q)9t+cmi%8@9mo3@@$t&a1ab*_tld%61#k#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
